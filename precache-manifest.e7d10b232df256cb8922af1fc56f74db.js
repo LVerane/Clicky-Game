362171fc54df5016b4f02d6022cec167e585a548
Lucio Verane <verane64@gmail.com>
2019-11-13 17:33:17 -0600

    Updates

---
+++ precache-manifest.e7d10b232df256cb8922af1fc56f74db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc2a986d32f7f871682096971b32bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a1628ecae8a701093c3",
+    "url": "/static/css/main.be3135db.chunk.css"
+  },
+  {
+    "revision": "6f2c9fdc8a67bcf18fcd",
+    "url": "/static/js/2.6a69dcdf.chunk.js"
+  },
+  {
+    "revision": "2a1628ecae8a701093c3",
+    "url": "/static/js/main.30aa674b.chunk.js"
+  },
+  {
+    "revision": "93178cac179f84ce7888",
+    "url": "/static/js/runtime-main.15eb2435.js"
+  }
+]);
